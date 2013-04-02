fb23d9f349308a5932723d223832f54e533c145f
Dr. M H B Ariyaratne <buddhika.ari@gmail.com>
2013-04-02 09:15:32 +0530

    Invenroty Items Finished

---
+++ src/java/gov/sp/health/facade/ConsumableItemFacade.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.sp.health.facade;
+
+import gov.sp.health.entity.ConsumableItem;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author www.divudi.com
+ */
+@Stateless
+public class ConsumableItemFacade extends AbstractFacade<ConsumableItem> {
+    @PersistenceContext(unitName = "HOPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ConsumableItemFacade() {
+        super(ConsumableItem.class);
+    }
+}
