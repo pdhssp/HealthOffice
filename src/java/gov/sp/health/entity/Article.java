76b2b3283bcdfb679109a33964c1770a8c7970d0
lahirumadushanka <lahirumadushanka2100@gmail.com>
2013-04-24 16:01:35 +0530

    article entity class

--- src/java/gov/sp/health/entity/Article.java
+++ src/java/gov/sp/health/entity/Article.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 
 /**
@@ -23,11 +24,32 @@ public class Article implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
-    @Temporal(javax.persistence.TemporalType.DATE)
-    Date retiredAt;
-    String retireComments;
-    String topic;
-    String content;
+     //Created Properties
+    @ManyToOne
+    private WebUser creater;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date createdAt;
+    //Retairing properties
+    private boolean retired;
+    @ManyToOne
+    private WebUser retirer;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date retiredAt;
+    private String retireComments;
+    private String sinhalaTopic;
+    private String tamilTopic;
+    private String englishTopic;
+    
+    private String sinhalaContent;
+    private String tamilContent;
+    private String englishContent;
+    
+    
+    
+    
+
+    
+    
 
     public Long getId() {
         
@@ -61,6 +83,102 @@ public class Article implements Serializable {
     @Override
     public String toString() {
         return "gov.sp.health.entity.Article[ id=" + id + " ]";
+    }
+
+    public WebUser getCreater() {
+        return creater;
+    }
+
+    public void setCreater(WebUser creater) {
+        this.creater = creater;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isRetired() {
+        return retired;
+    }
+
+    public void setRetired(boolean retired) {
+        this.retired = retired;
+    }
+
+    public WebUser getRetirer() {
+        return retirer;
+    }
+
+    public void setRetirer(WebUser retirer) {
+        this.retirer = retirer;
+    }
+
+    public Date getRetiredAt() {
+        return retiredAt;
+    }
+
+    public void setRetiredAt(Date retiredAt) {
+        this.retiredAt = retiredAt;
+    }
+
+    public String getRetireComments() {
+        return retireComments;
+    }
+
+    public void setRetireComments(String retireComments) {
+        this.retireComments = retireComments;
+    }
+
+    public String getSinhalaTopic() {
+        return sinhalaTopic;
+    }
+
+    public void setSinhalaTopic(String sinhalaTopic) {
+        this.sinhalaTopic = sinhalaTopic;
+    }
+
+    public String getTamilTopic() {
+        return tamilTopic;
+    }
+
+    public void setTamilTopic(String tamilTopic) {
+        this.tamilTopic = tamilTopic;
+    }
+
+    public String getEnglishTopic() {
+        return englishTopic;
+    }
+
+    public void setEnglishTopic(String englishTopic) {
+        this.englishTopic = englishTopic;
+    }
+
+    public String getSinhalaContent() {
+        return sinhalaContent;
+    }
+
+    public void setSinhalaContent(String sinhalaContent) {
+        this.sinhalaContent = sinhalaContent;
+    }
+
+    public String getTamilContent() {
+        return tamilContent;
+    }
+
+    public void setTamilContent(String tamilContent) {
+        this.tamilContent = tamilContent;
+    }
+
+    public String getEnglishContent() {
+        return englishContent;
+    }
+
+    public void setEnglishContent(String englishContent) {
+        this.englishContent = englishContent;
     }
     
 }
